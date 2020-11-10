6a15ba0abf8874093a46af1f996ec9f79b823780
Mạnh NV <manhnv229@gmail.com>
2020-11-10 16:16:42 +0700

    Update JS

---
+++ NVManh/MISA.AMIS.Web/js/page/employee.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+
+
+function loadData() {
+    // Lấy dữ liệu về:
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET",
+    }).done(function (res) {
+        var data = res;
+        debugger;
+    }).fail(function (res) {
+
+    })
+    // binding dữ liệu lên table:
+
+}
