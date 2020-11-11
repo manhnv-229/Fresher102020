0f75c54051fa18640fd7799283c13fe9ce2ad589
Mạnh NV <manhnv229@gmail.com>
2020-11-11 17:45:33 +0700

    JS buổi 2 - loadData

--- NVManh/MISA.AMIS.Web/js/page/customer.js
+++ NVManh/MISA.AMIS.Web/js/page/customer.js
@@ -19,21 +19,17 @@ function loadData() {
             salary = formatMoney(salary);
             dateOfBirth = formatDate(dateOfBirth);
             var checkbox = `<input type="checkbox"/>`;
-            if (item.Gender>0) {
+            if (item.Gender > 0) {
                 var checkbox = `<input type="checkbox" checked/>`;
             }
             var tr = $(`<tr>
                         <td><div><span>`+ item.EmployeeCode + `</span></div></td>
                         <td><div><span>`+ item['FullName'] + `</span></div></td>
-                        <td><div class="text-align-center">`+ checkbox +`</div></td>
+                        <td><div class="text-align-center">`+ checkbox + `</div></td>
                         <td><div><span>`+ dateOfBirth + `</span></div></td>
                         <td><div><span>`+ item['PhoneNumber'] + `</span></div></td>
                         <td><div><span>`+ item['Email'] + `</span></div></td>
-                        <td><div><span>`+ item['PositionName'] + `</span></div></td>
-                        <td><div><span>`+ item['DepartmentName'] + `</span></div></td>
-                        <td><div class="text-align-right"><span>`+ salary + `</span></div></td>
                         <td><div style="max-width:250px" title="`+ item['Address'] + `"><span>` + item['Address'] + `</span></div></td>
-                        <td><div><span>`+ item['WorkStatusName'] + `</span></div></td>
                     </tr>`);
             $('table tbody').append(tr);
             debugger;
@@ -44,3 +40,4 @@ function loadData() {
     // binding dữ liệu lên table:
 
 }
+
