2bd2958559f1dfb1628d12e43506c6a9b57bd64a
ngadt.ptit <ngadt.ptit@gmail.com>
2020-11-12 17:00:21 +0700

    Sửa lại table, tạo base loadData()

---
+++ DTNGA/MISA.AMIS.Web/js/common/common.js
@@ -0,0 +1,26 @@
+﻿
+
+/**
+ * Hàm xử lý định dạng ngày
+ * @param {any} date Bất kì kiểu dữ liệu nào
+ * Creared by dtnga (11/11/2020)
+ */
+function formatDate(date) {
+    if (Date.IsNaN)
+        return ""
+    else {
+        var date = new Date(date);
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        day = day<0 ? '0'+day : day;
+        return day + "/" + month + "/" + year;
+    }
+}
+/**
+ * Hàm xử lý dữ liệu tiền tệ
+ * @param {int} money Số tiền
+ */
+function formatMoney(money) {
+
+}
---
+++ DTNGA/MISA.AMIS.Web/js/page/customer.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+    var customerjs = new CustomerJS();
+})
+
+class CustomerJS extends BaseJS {
+    constructor() {
+        super();
+    }
+
+    getUrl() {
+        return "http://api.manhnv.net/api/customers";
+    }
+}
