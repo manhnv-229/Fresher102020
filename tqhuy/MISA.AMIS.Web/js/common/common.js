4c93af3834510ec7befb93ca86eaa6ee160ec9a6
TranHuy1403 <quochuy140398@gmail.com>
2020-11-12 08:26:08 +0700

    update- load data table - dialog

--- tqhuy/MISA.AMIS.Web/js/common/common.js
+++ tqhuy/MISA.AMIS.Web/js/common/common.js
@@ -1,1 +1,22 @@
-﻿
+﻿class Base {
+    /** hàm lấy dữ liệu đẩy lên table
+     * thHuy 12-11-2020
+     * */
+    getData() {
+        var data = arguments[0];
+        var fields = $("[fieldName]");
+        $.each(data, function (index, item) {
+            var rowtable = $("<tr></tr>");
+            $.each(fields, function (i, field) {
+                var fieldName = field.getAttribute('fieldName');
+                if (fieldName != "index") {
+                    rowtable = rowtable.append(`<td>${item[fieldName]}</td>`);
+                }
+                else {
+                    rowtable = rowtable.append(`<td>${index + 1}</td>`);
+                }
+            });
+            $("tbody").append(rowtable);
+        });
+    }
+}
--- tqhuy/MISA.AMIS.Web/js/employee/employee.js
+++ tqhuy/MISA.AMIS.Web/js/employee/employee.js
@@ -1,1 +1,46 @@
-﻿
+﻿/// <reference path="../common/common.js" />
+
+
+$(document).ready(function () {
+    getdata();
+
+    $("#show-dialog").click(function () {
+        $("#dialog").show();
+    });
+
+    $('td').click(function () {
+        var col = $(this).parent().children().index($(this));
+        var row = $(this).parent().parent().children().index($(this).parent());
+        console.log(row)
+        alert('Row: ' + row + ', Column: ' + col);
+    });
+
+    $(".page-table").click(function () {
+        $(this).addClass("page-selected");
+        $(this).siblings().removeClass("page-selected");
+        
+    })
+
+    $(".btn-cancel").click(function () {
+        $("#dialog").hide();
+    })
+}) 
+
+/** hàm gọi api trả về dữ liệu rồi đẩy lên table
+ * tqhuy 12/11/2020
+ * */
+function getdata() {
+    $.ajax({
+        url: "http://api.manhnv.net/api/employees",
+        method: "GET",
+        dataType: "JSON",
+        success: function (reponse) {
+            var base = new Base();
+            base.getData(reponse);
+        },
+        error: function (err) {
+            alert("Có lỗi! Không thể load được dữ liệu!");
+        }
+    });
+}
+
