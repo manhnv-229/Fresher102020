b59fa6ce20ce1c33ba0804e2dda78568f6b66ccc
TungAnh <tunganhh239@gmail.com>
2020-11-13 09:33:01 +0700

    Cập nhật phân trang,  customers

---
+++ HTAnh/MISA.AMIS/js/page/customers.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    new CustomersJS();
+});
+
+class CustomersJS extends BaseJS {
+    constructor() {
+        super();
+    }
+    setDataUrl() {
+        this.getDataUrl = "http://api.manhnv.net/api/customers";
+    }
+}
