e63f8f412be76cb73f8f86d9280ebad65fc8d49f
Phương Trung Đức <phgtrungduc@gmail.com>
2020-11-20 22:53:56 +0700

    Hoàn thành bài tap

--- ptduc/js/page/customer.js
+++ ptduc/js/page/customer.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  var customer = new CustomerJS();
+  new CustomerJS();
 });
 
 class CustomerJS extends BaseJS {
   constructor() {
     super()
-    this.setLinkAPI("http://api.manhnv.net/api/customers");
+    this.router = "customers";
     this.loadData();
   }
 }
--- ptduc/js/page/employee.js
+++ ptduc/js/page/employee.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  var employee = new EmployeeJS();
+  new EmployeeJS();
 });
 
 class EmployeeJS extends BaseJS {
   constructor() {
     super();
-    this.setLinkAPI("http://api.manhnv.net/api/employees");
+    this.router = "employees";
     this.loadData();
   }
 }
