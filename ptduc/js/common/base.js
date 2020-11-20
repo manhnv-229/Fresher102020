75391135f882de390e40bf5606d0b1f73d99c84a
Phương Trung Đức <phgtrungduc@gmail.com>
2020-11-20 23:00:36 +0700

    Revert "Hoàn thành bài tap"
    This reverts commit 8712a927d5c1122d852750aea59b4f325927e051.

--- ptduc/js/common/base.js
+++ ptduc/js/common/base.js
@@ -14,7 +14,7 @@ class BaseJS {
     var self = this;
     $(".sync-m-btn").click(function () {
       console.log("ấn");
-      self.showNotification("check", "fail");
+      self.showNotification("con cặc", "fail");
     });
     //Ấn nút hủy trên thông báo xóa entity
     this.cancleDelete();
