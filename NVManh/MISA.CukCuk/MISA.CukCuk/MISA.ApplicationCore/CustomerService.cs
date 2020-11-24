205fd742296957de4c7b7d677bb5d0b83a4b9357
Mạnh NV <manhnv229@gmail.com>
2020-11-24 16:47:42 +0700

    Phân chia Project

---
+++ NVManh/MISA.CukCuk/MISA.CukCuk/MISA.ApplicationCore/CustomerService.cs
@@ -0,0 +1,73 @@
+﻿using MISA.ApplicationCore.Entities;
+using MISA.Entity;
+using MISA.Infarstructure;
+using MISA.Infarstructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.ApplicationCore
+{
+    public class CustomerService
+    {
+        #region Method
+        // Lấy dánh sách khách hàng:
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var customerContext = new CustomerContext();
+            var customers = customerContext.GetCustomers();
+            return customers;
+        }
+
+        // Thêm mới khách hàng:
+        public ServiceResult InsertCustomer(Customer customer)
+        {
+            var serviceResult = new ServiceResult();
+            var customerContext = new CustomerContext();
+            // Validate dữ liệu:
+            // Check trường bắt buộc nhập,nếu dữ liệu chưa hợp lệ thì trả về mô tả lỗi::
+            var customerCode = customer.CustomerCode;
+            if (string.IsNullOrEmpty(customerCode))
+            {
+                var msg = new
+                {
+                    devMsg = new { fieldName = "CustomerCode", msg = "Mã khách hàng không được phép để trống," },
+                    userMsg = "Mã khách hàng không được phép để trống",
+                    Code = MISACode.NotValid,
+                };
+                serviceResult.MISACode = MISACode.NotValid;
+                serviceResult.Messenger = "Mã khách hàng không được phép để trống.";
+                serviceResult.Data = msg;
+                return serviceResult;
+            }
+
+            // Check trùng mã:
+
+            var res = customerContext.GetCustomerByCode(customerCode);
+            if (res != null)
+            {
+                var msg = new
+                {
+                    devMsg = new { fieldName = "CustomerCode", msg = "Mã khách hàng đã tồn tại" },
+                    userMsg = "Mã khách hàng đã tồn tại",
+                    Code = MISACode.NotValid,
+                };
+                serviceResult.MISACode = MISACode.NotValid;
+                serviceResult.Messenger = "Mã khách hàng không được phép để trống.";
+                serviceResult.Data = msg;
+                return serviceResult;
+            }
+
+            // Thêm mới khi dữ liệu đã hợp lệ:
+            var rowAffects = customerContext.InsertCustomer(customer);
+            serviceResult.MISACode = MISACode.IsValid;
+            serviceResult.Messenger = "Thêm thành công";
+            serviceResult.Data = rowAffects;
+            return serviceResult;
+        }
+        // Sửa thông tin khách hàng:
+
+        // Xóa khách hàng:
+        #endregion
+    }
+}
--- NVManh/MISA.CukCuk/MISA.CukCuk/MISA.CukCuk.Web/Controllers/CustomersController.cs
+++ NVManh/MISA.CukCuk/MISA.CukCuk/MISA.CukCuk.Web/Controllers/CustomersController.cs
@@ -4,9 +4,11 @@ using Dapper;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MISA.CukCuk.Web.Models;
 using System.Data;
 using MySql.Data.MySqlClient;
+using MISA.ApplicationCore;
+using MISA.Infarstructure.Models;
+using MISA.Entity;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,9 +30,8 @@ namespace MISA.CukCuk.Web.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var connectionString = "User Id=dev;Host=35.194.135.168;Port=3306;Database=MISACukCuk; Password=12345678@Abc;Character Set=utf8";
-            IDbConnection dbConnection = new MySqlConnection(connectionString);
-            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.StoredProcedure);
+            var customerService = new CustomerService();
+            var customers =  customerService.GetCustomers();
             return Ok(customers);
         }
 
@@ -54,45 +55,11 @@ namespace MISA.CukCuk.Web.Controllers
         [HttpPost]
         public IActionResult Post(Customer customer)
         {
-            // Validate dữ liệu:
-            // Check trường bắt buộc nhập:
-            var customerCode = customer.CustomerCode;
-            if (string.IsNullOrEmpty(customerCode))
-            {
-                var msg = new
-                {
-                    devMsg = new { fieldName = "CustomerCode", msg = "Mã khách hàng không được phép để trống," },
-                    userMsg = "Mã khách hàng không được phép để trống",
-                    Code = 999,
-                };
-                return BadRequest(msg);
-            }
-            // Check trùng mã:
-            var connectionString = "User Id=dev;Host=35.194.135.168;Port=3306;Database=MISACukCuk; Password=12345678@Abc;Character Set=utf8";
-            IDbConnection dbConnection = new MySqlConnection(connectionString);
-            var res = dbConnection.Query<Customer>("Proc_GetCustomerByCode", new { CustomerCode = customerCode }, commandType: CommandType.StoredProcedure);
-            if (res.Count() > 0)
-                return BadRequest("Mã đã tồn tại");
-            var properties = customer.GetType().GetProperties();
-            var parameters = new DynamicParameters();
-            foreach (var property in properties)
-            {
-                var propertyName = property.Name;
-                var propertyValue = property.GetValue(customer);
-                var propertyType = property.PropertyType;
-                if (propertyType == typeof(Guid) || propertyType == typeof(Guid?))
-                {
-                    parameters.Add($"@{propertyName}", propertyValue, DbType.String);
-                }
-                else
-                {
-                    parameters.Add($"@{propertyName}", propertyValue);
-                }
-
-            }
-
-            var rowAffects = dbConnection.Execute("Proc_InsertCustomer", parameters, commandType: CommandType.StoredProcedure);
-            if (rowAffects > 0)
+            var customerService = new CustomerService();
+            var serviceResult = customerService.InsertCustomer(customer);
+            if (serviceResult.MISACode == MISACode.NotValid)
+                return BadRequest(serviceResult.Data);
+            if (serviceResult.MISACode == MISACode.IsValid && (int)serviceResult.Data > 0)
                 return Created("adfd", customer);
             else
                 return NoContent();
