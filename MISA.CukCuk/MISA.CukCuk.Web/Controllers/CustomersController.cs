6202be32b64d3458237a94620d35c35f7acd97f7
Tuan Hai <tuanhai20062000@gmail.com>
2020-11-24 14:38:40 +0700

    add service

---
+++ MISA.CukCuk/MISA.CukCuk.Web/Controllers/CustomersController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MISA.CukCuk.Web.Models;
+using MySql.Data.MySqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MISA.CukCuk.Web.Controllers
+{
+    /// <summary>
+    /// Api danh mục khách hàng
+    /// CreatedBy: LTHAI(23/11/2020)
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public readonly string connectionString = @"User Id=dev;Host=35.194.135.168;Port=3306;Database=MISACukCuk;Password=12345678@Abc;Character Set=utf8;";
+        /// <summary>
+        /// Lấy toàn bộ danh sách khách hàng
+        /// </summary>
+        /// <returns>Danh sách khách hàng</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.StoredProcedure);
+            return Ok(customers);
+        }
+
+        /// <summary>
+        /// Lấy khách hàng theo CustomerId
+        /// </summary>
+        /// <param name="id">CustomerId</param>
+        /// <returns>Khách mới CustomerId tương ứng</returns>
+        /// CreatedBy: LTHAI(23/11/2020)
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {   
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var customer = dbConnection.Query<Customer>("Proc_GetCustomerById", new { CustomerId = id}, commandType: CommandType.StoredProcedure);
+            return Ok(customer);
+        }
+
+        /// <summary>
+        /// Thêm mới một khách hàng
+        /// </summary>
+        /// <param name="customer">Khách hàng mới</param>
+        /// <returns>
+        /// + Thành công : Trả về khách hàng mới
+        /// + Không thành công: BadRequest 
+        /// </returns>
+        /// CreatedBy: LTHAI(23/11/2020)
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            // Validate dữ liệu trường bắt buộc
+            var customerCode = customer.CustomerCode;
+            if (string.IsNullOrEmpty(customerCode))
+            
+            {
+                var msg = new {
+                    devMsg = new { fieldName = "CustomerCode", Msg = "Trường customerCode không được để trống" },
+                    userMsg = "Trường customerCode không được để trống",
+                    errorCode = 999
+                };
+                return BadRequest(msg);
+            }
+            // Validate trùng CustomerCode
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var result = dbConnection.Query("Proc_GetCustomerByCode", new {CustomerCode = customerCode }, commandType: CommandType.StoredProcedure);
+            if(result.Count() > 0)
+            {
+                var msg = new
+                {
+                    devMsg = new { fieldName = "CustomerCode", Msg = "Trường customerCode đã tồn tại" },
+                    userMsg = "Trường customerCode đã tồn tại",
+                    errorCode = 400
+                };
+                return BadRequest(msg);
+            }
+            // Đổi kiểu Guid thành string nếu có
+            var properties = customer.GetType().GetProperties();
+            var parameters = new DynamicParameters();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetValue(customer);
+                var propertyType = property.PropertyType;
+                if (propertyType == typeof(Guid) || propertyType == typeof(Guid?))
+                {
+                    if(propertyName == "CustomerId")
+                    {
+                        parameters.Add(propertyName, Guid.NewGuid(), DbType.String);
+                    }
+                    else
+                    {
+                        parameters.Add(propertyName, propertyValue, DbType.String);
+                    }
+                    
+                }
+                else
+                {
+                    parameters.Add(propertyName, propertyValue);
+                }
+            }
+            // Thực hiện thêm khách hàng
+            var rowAffects = dbConnection.Execute("Proc_InsertCustomer", parameters, commandType: CommandType.StoredProcedure);
+            if (rowAffects > 0)
+            {
+                return Created("Success", parameters);
+            }
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Cập nhật một khách hàng
+        /// </summary>
+        /// <param name="id">CustomerId</param>
+        /// <param name="value">Thông tin khách hàng được cập nhật</param>
+        /// <returns>
+        /// + Thành công : Trả về Ok
+        /// + Không thành công: NoContent
+        /// </returns>
+        /// CreatedBy: LTHAI(23/11/2020)
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] Customer customer)
+        {
+            // Đổi kiểu Guid thành string nếu có
+            var properties = customer.GetType().GetProperties();
+            var parameters = new DynamicParameters();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetValue(customer);
+                var propertyType = property.PropertyType;
+                if (propertyType == typeof(Guid?))
+                {
+                    parameters.Add(propertyName, propertyValue, DbType.String);
+                }
+                else if(propertyType == typeof(Guid)){
+                    parameters.Add(propertyName, id);
+                }
+                else if (propertyType == typeof(bool))
+                {
+                    
+                    parameters.Add(propertyName, propertyValue, DbType.SByte);
+                }
+                else
+                {
+                    parameters.Add(propertyName, propertyValue);
+                }
+            }
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var rowAffects = dbConnection.Execute("Proc_UpdateCustomer", parameters, commandType: CommandType.StoredProcedure);
+            if(rowAffects > 0)
+            {
+                return Ok(rowAffects);
+            }
+            return NoContent();
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MISA.CukCuk/MISA.CukCuk.Web/Models/Customer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Web.Models
+{
+    /// <summary>
+    /// Khách hàng
+    /// CreatedBy: LTHAI(23/11/2020)
+    /// </summary>
+    public class Customer
+    {
+        #region Declare
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+        /// <summary>
+        /// Họ
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Tên
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Họ và tên
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Giới tính
+        /// </summary>
+        public int? Gender { get; set; }
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Ngày sinh
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+        /// <summary>
+        /// Hòm thư của khách hàng
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Mã nhóm khách hàng
+        /// </summary>
+        public Guid? CustomerGroupId { get; set; }
+        /// <summary>
+        /// Mã thẻ thành viên
+        /// </summary>
+        public string MemberCardCode { get; set; }
+        /// <summary>
+        /// Tên công ty
+        /// </summary>
+        public string CompanyName { get; set; }
+        /// <summary>
+        /// Mã số thuế của công ty
+        /// </summary>
+        public string CompanyTaxCode { get; set; }
+        /// <summary>
+        /// Số tiền nợ
+        /// </summary>
+        public double? DebitAmount { get; set; }
+        /// <summary>
+        /// Dừng theo dõi
+        /// </summary>
+        public bool? IsStopFollow { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }   
+        public DateTime? ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        #endregion
+
+        #region Method
+        #endregion
+
+    }
+}
